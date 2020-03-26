3c1eece0a70d3cdf73d8a88a156709c3ced7ee2f
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-26 10:09:24 -0600

    Add conditiojnal rendering of Menu and data fetch

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getAllData } from '../Utils/APICalls';
 import ResourceForm from '../ResourceForm/ResourceForm';
+import ResourceItem from '../ResourceItem/ResourceItem'
+
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      resources: [],
+      isMenuOpen:true
+    };
   }
+
+  componentDidMount() {
+    this.getDataWithConnection()
+  }
+
+  getDataWithConnection = async () => {
+    let response = await getAllData();
+    if (response === undefined) {
+      response = []
+    }
+    this.setState({ resources: response });
+  };
+
   render() {
+    let {resources, isMenuOpen} = this.state
+    let resourceItemsList = []
+    if (resources.length!==0){
+      resourceItemsList = resources.map(el => <ResourceItem resourse={el}/>)
+    }
+
     return (
       <div className='App'>
-        <ResourceForm />
-        {/* <ResoueceList /> */}
+        {isMenuOpen && <ResourceForm />}
+        {resourceItemsList}
       </div>
     );
   }
