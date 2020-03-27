8653c2a69ca932b5e5600d2e8913d5f7f804ba0a
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-27 16:51:13 -0600

    Change table to editable content

--- src/ResourceItem/ResourceItem.js
+++ src/ResourceItem/ResourceItem.js
@@ -8,16 +8,46 @@ return (
     <td>
       <button type="button" className="save-btn">Save Changes</button>
     </td>
-    <td>{name}</td>
-    <td>{website}</td>
-    <td>{street}</td>
-    <td>{city}</td>
-    <td>{state}</td>
-    <td>{zip_code}</td>
-    <td>{contact}</td>
-    <td>{notes}</td>
-    <td>{category}</td>
-    <td>{subcategory}</td>
+    <td><div contenteditable='true' >
+      {name}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {website}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {street}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {city}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {state}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {zip_code}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {contact}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {notes}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {category}
+      </div>
+      </td>
+    <td><div contenteditable='true' >
+      {subcategory}
+      </div>
+      </td>
   </tr>
 );
 }
