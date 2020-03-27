e1048db174d1ae2f3ca9c4a7ad9fb8011fdaca84
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-27 15:09:16 -0600

    Updates

---
+++ precache-manifest.a0d681cc1fadeb96fe1ff6dd65cbc68c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc427ef7ac4cee4b516f1725928cf6f",
+    "url": "/wbp-admin-site/index.html"
+  },
+  {
+    "revision": "1efac6973c0ac6ba7884",
+    "url": "/wbp-admin-site/static/css/main.31fa8cbb.chunk.css"
+  },
+  {
+    "revision": "2b68e986dccc27959123",
+    "url": "/wbp-admin-site/static/js/2.d930f2e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wbp-admin-site/static/js/2.d930f2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efac6973c0ac6ba7884",
+    "url": "/wbp-admin-site/static/js/main.f9b85263.chunk.js"
+  },
+  {
+    "revision": "0ab7f52776aab5930876",
+    "url": "/wbp-admin-site/static/js/runtime-main.fd4744aa.js"
+  },
+  {
+    "revision": "bddc9f479a1ae4fec5e66f111ef2a8c8",
+    "url": "/wbp-admin-site/static/media/header_logo.bddc9f47.svg"
+  },
+  {
+    "revision": "fd374feebd2a0c167308898800c5d19c",
+    "url": "/wbp-admin-site/static/media/plus.fd374fee.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wbp-admin-site/precache-manifest.a0d681cc1fadeb96fe1ff6dd65cbc68c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wbp-admin-site/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
